8147a3f19677b72338411012f955733d2cc48071
sallop <shiogai1987@yahoo.co.jp>
2016-02-01 07:33:22 +0900

    add scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   module: {
     loaders: [
+      { test: /\.scss$/, loader: 'style!css!sass' },
       { test: /\.css/, loader: "style!css" },
       { test: /\.ts/, loader: "ts" },
     ]
